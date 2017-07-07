8c7d473d57cc8d8e704695ea23faba7b7c8fb001
skandru <kandru.suresh@gmail.com>
2017-07-06 19:51:58 -0700

    Updated main.ts file [iss1]

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 "use strict";
 exports.__esModule = true;
+var $ = require("jquery");
 var getcoffee_1 = require("./getcoffee");
 var SweetSweetClass = (function () {
     function SweetSweetClass() {
         console.log("Even sweeter");
+        $('body').css('background-color', 'red');
     }
     return SweetSweetClass;
 }());
